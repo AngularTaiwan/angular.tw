88412fcd6c7af89082b4f2a41a0afa159c1ee8b3
Will <doggy.huang@gmail.com>
2019-02-22 02:45:35 +0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+OPENCC=/usr/share/opencc
+
+echo '建立 STPhrases-Angular.ocd 檔案'
+##########################################
+rm -f TEMP-STPhrasesAngular.txt
+cat ./data/dictionary/STPhrases.txt    >> TEMP-STPhrasesAngular.txt
+cat ./STPhrases-Angular.txt            >> TEMP-STPhrasesAngular.txt
+sudo opencc_dict -f text -t ocd -i TEMP-STPhrasesAngular.txt -o $OPENCC/STPhrases-Angular.ocd
+rm -f TEMP-STPhrasesAngular.txt
+
+echo '建立 TWPhrases-Angular.ocd 檔案'
+##########################################
+rm -f TEMP-TWPhrasesAngular.txt
+cat ./TWPhrases-Angular.txt              >> TEMP-TWPhrasesAngular.txt
+cat ./TWPhrases-Other.txt                >> TEMP-TWPhrasesAngular.txt
+cat ./data/dictionary/TWPhrasesIT.txt    >> TEMP-TWPhrasesAngular.txt
+cat ./data/dictionary/TWPhrasesOther.txt >> TEMP-TWPhrasesAngular.txt
+sudo opencc_dict -f text -t ocd -i TEMP-TWPhrasesAngular.txt -o $OPENCC/TWPhrases-Angular.ocd
+rm -f TEMP-TWPhrasesAngular.txt
+
+echo "複製 s2twp-Angular.json 到 $OPENCC"
+##########################################
+sudo cp s2twp-Angular.json $OPENCC/
+
+echo "測試結果"
+##########################################
+cd ~/build/ng-docs.github.io
+git reset --hard
+
+cd guide
+find . -name 'rx*.html' -type f -exec echo '{}' \; -exec opencc -c s2twp-Angular -i '{}' -o '{}' \;
+
+cd ../generated/docs/guide
+find . -name 'rx*.json' -type f -exec echo '{}' \; -exec opencc -c s2twp-Angular -i '{}' -o '{}' \;
+
+# lite-server
